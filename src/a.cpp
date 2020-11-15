c655ced7ff5e5e05726aeb0b99c741c156ddd78a
Nicholas <me@cheeze2000.net>
2020-11-15 11:47:03 +0800

    feat: lists

---
+++ src/a.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define DEBUG(line) \
+  line; \
+  cout << #line << ";\n"; \
+  pretty_print(xs);
+
+void pretty_print(forward_list<int>& xs) {
+  cout << "xs: [";
+  for (auto i = xs.begin(); i != xs.end(); i++) {
+    if (i != xs.begin()) cout << ", ";
+    cout << *i;
+  }
+  cout << "]\n\n";
+}
+
+int main() {
+  forward_list<int> xs { 0, 1, 5, 2, 4, 3 };
+  pretty_print(xs);
+
+  DEBUG(xs.front() += 6);
+  DEBUG(xs.push_front(3));
+  DEBUG(xs.sort());
+  DEBUG(xs.reverse());
+  DEBUG(xs.pop_front());
+  DEBUG(xs.unique());
+}
